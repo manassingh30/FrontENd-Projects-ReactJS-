62d474c6ac062d217986125e4097bf6ee174edb3
akashrajakku <akashrajakku263@gmail.com>
2024-04-24 00:21:44 +0530

    productinfo

--- UrbanMarket/src/App.jsx
+++ UrbanMarket/src/App.jsx
@@ -21,6 +21,9 @@ import Cart from './pages/cart/Cart';
 import Dashboard from './pages/admin/dashboard/Dashboard';
 import NoPage from './pages/nopage/NoPage';
 import MyState from './context/data/myState';
+import Login from './pages/registration/Login';
+import Signup from './pages/registration/Signup';
+import ProductInfo from './pages/productInfo/ProductInfo';
 
 function App() {
   return (
@@ -31,6 +34,9 @@ function App() {
           <Route path="/order" element={<Order />}/>
           <Route path="/cart" element={<Cart />}/>
           <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path='/login' element= {<Login />} />
+          <Route path='/signup' element= {<Signup />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
           <Route path="/*" element={<NoPage />}/>
         </Routes>
       </Router>
