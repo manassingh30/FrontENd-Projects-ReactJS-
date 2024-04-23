59c7da0199e1f875cb12682da7179e6ce47033b8
akashrajakku <akashrajakku263@gmail.com>
2024-04-23 23:53:42 +0530

    uptoFooter

--- UrbanMarket/src/pages/home/Home.jsx
+++ UrbanMarket/src/pages/home/Home.jsx
@@ -1,13 +1,21 @@
 import React, {useContext} from 'react'
 import Layout from '../../components/layout/Layout'
-import myContext from '../../context/data/myContext'
+import MyContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Testimonial from '../../components/testimonial/testimonial'
+
 
 function Home() {
 
   
   return (
     <Layout>
-       Home
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Testimonial />
     </Layout>
   )
 }
