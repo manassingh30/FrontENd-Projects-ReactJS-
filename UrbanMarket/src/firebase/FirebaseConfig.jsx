bf41daa30dac6b30eecf12e81feaaae71592cdfc
akashrajakku <akashrajakku263@gmail.com>
2024-05-04 20:02:38 +0530

    signup

--- UrbanMarket/src/firebase/FirebaseConfig.jsx
+++ UrbanMarket/src/firebase/FirebaseConfig.jsx
@@ -1,25 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: conf.apiKey,
-  authDomain: conf.authDomain,
-  projectId: conf.projectId,
-  storageBucket: conf.storageBucket,
-  messagingSenderId: conf.messagingSenderId,
-  appId: conf.appId
+  apiKey: "AIzaSyAo-IiQ5LsnYCRn5XdSJw4l8zQoYmkp-js",
+  authDomain: "urbanmarket-b81a3.firebaseapp.com",
+  projectId: "urbanmarket-b81a3",
+  storageBucket: "urbanmarket-b81a3.appspot.com",
+  messagingSenderId: "299649570329",
+  appId: "1:299649570329:web:12283e5eb563126fb47c88"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const fireDB = getFirestore(app);
 const auth = getAuth(app)
 export {fireDB,auth } ;
-
-//react toastify shows alert like sinup done, password reset kind of thing
--- UrbanMarket/src/pages/registration/Signup.jsx
+++ UrbanMarket/src/pages/registration/Signup.jsx
@@ -1,62 +1,64 @@
-import { useContext, useState } from 'react'
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom'
-import myContext from '../../context/data/myContext'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-import { auth, fireDB } from '../../fireabase/FirebaseConfig';
+import myContext from '../../context/data/myContext';
+import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, fireDB } from '../../firebase/FirebaseConfig';
 import { Timestamp, addDoc, collection } from 'firebase/firestore';
 import Loader from '../../components/loader/Loader';
 
 function Signup() {
-   const [name, setName] = useState("")
-   const [email, setEmail] = useState("")
-   const [password, setPassword] = useState("")
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-   const context = useContext(myContext)
-   const{loading, setLoading} = context;
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
 
-   const signup = async () => {
-    setLoading(true)
-    if (name === "" || email === "" || password === "") {
-        return toast.error("All fields are mandatory")
-    }
-
-    try {
-        const users = await createUserWithEmailAndPassword(auth, email, password);
-
-        const user = {
-            name: name,
-            uid: users.user.uid,
-            email: users.user.email,
-            time : Timestamp.now()
+    const signup = async () => {
+        setLoading(true)
+        if (name === "" || email === "" || password === "") {
+            return toast.error("All fields are required")
         }
-        const userRef = collection(fireDB, "users")
-        await addDoc(userRef, user);
-        toast.success("Signup Succesfully")
-        setName("");
-        setEmail("");
-        setPassword("");
-        setLoading(false)
-        
-    } catch (error) {
-        console.log(error)
-        setLoading(false)
-    }
-}
 
+        try {
+            const users = await createUserWithEmailAndPassword(auth, email, password);
+
+            console.log(users)
+
+            const user = {
+                name: name,
+                uid: users.user.uid,
+                email: users.user.email,
+                time : Timestamp.now()
+            }
+            const userRef = collection(fireDB, "users")
+            await addDoc(userRef, user);
+            toast.success("Signup Succesfully")
+            setName("");
+            setEmail("");
+            setPassword("");
+            setLoading(false)
+            
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
 
     return (
-        <div className=' flex justify-center items-center h-screen bg-whitesmoke'>
-            <div className=' bg-gradient-to-b from-purple-600 to-purple-400 px-10 py-10 rounded-xl '>
+        <div className=' flex justify-center items-center h-screen'>
+            {loading && <Loader/>}
+            <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
                 <div className="">
-                    <h1 className='text-center text-black text-xl mb-4 font-bold'>Signup</h1>
+                    <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
                 </div>
-
                 <div>
                     <input type="text"
                         value={name}
-                        onChange={(e)=> setName(e.target.value)}
+                        onChange={(e) => setName(e.target.value)}
                         name='name'
-                        className=' bg-whitesmoke mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-gray-400 outline-none'
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Name'
                     />
                 </div>
@@ -64,33 +66,30 @@ function Signup() {
                 <div>
                     <input type="email"
                         value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                         name='email'
-                        className=' bg-whitesmoke mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-gray-400 outline-none'
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
                 </div>
-
                 <div>
                     <input
                         type="password"
                         value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
-                        className=' bg-whitesmoke mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-gray-400 outline-none'
+                        onChange={(e) => setPassword(e.target.value)}
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
-
                 <div className=' flex justify-center mb-3'>
                     <button
                         onClick={signup}
-                        className=' bg-white w-full text-black font-medium px-2 py-2 rounded-lg'>
+                        className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
                         Signup
                     </button>
                 </div>
-
                 <div>
-                    <h2 className='text-white'>Already have an account? <Link className=' text-white font-bold' to={'/login'}>Login</Link></h2>
+                    <h2 className='text-white'>Have an account <Link className=' text-red-500 font-bold' to={'/login'}>Login</Link></h2>
                 </div>
             </div>
         </div>
