603e68ce69a8a271cf69f75369bde8ef584495af
akashrajakku <akashrajakku263@gmail.com>
2024-05-05 17:34:41 +0530

    stable upto updateproduct and deleteproduct

--- UrbanMarket/src/context/data/myState.jsx
+++ UrbanMarket/src/context/data/myState.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
-import myContext from './myContext'
+import React, { useEffect, useState } from 'react'
+import MyContext from './myContext';
+import { fireDB } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 function myState(props) {
   const [mode, setMode] = useState('light');
-
+  const [loading, setLoading] = useState(false);
+  
   const toggleMode= () =>{
     if(mode==='light'){
       setMode('dark');
@@ -15,12 +19,119 @@ function myState(props) {
   }
   }
 
-  const [loading, setLoading] = useState(false)
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+  })    
+
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error('Please fill all fields')
+    }
+    const productRef = collection(fireDB, "products")
+    setLoading(true)
+    try {
+      await addDoc(productRef, products)
+      toast.success("Product Added successfully")
+      setTimeout( ()=>{
+        window.location.href= '/dashboard'
+      }, 800);
+      
+      getProductData()
+      closeModal()
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+    setProducts("")
+  }
+
+  const [product, setProduct] = useState([]);
+
+  const getProductData = async () => {
+    setLoading(true)
+    try {
+      const q = query(
+        collection(fireDB, "products"),
+        orderBy("time"),
+      );
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productsArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productsArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProduct(productsArray)
+        setLoading(false);
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  //update product
+
+  const edithandle= (item)=>{
+    setProducts(item)
+  }
+
+  const updateProduct= async(item)=>{
+    setLoading(true)
+  
+  try {
+    await setDoc(doc(fireDB, "products", products.id), products);
+    toast.success("Product Updated successfully")
+    setTimeout(() => {
+      window.location.href = '/dashboard'
+    }, 800);
+    
+    getProductData();
+    setLoading(false)
+    
+  } catch (error) {
+    setLoading(false)
+    console.log(error)
+  }
+  setProducts("")
+}
+
+//deleteProduct
+
+const deleteProduct = async (item) => {
+  setLoading(true)
+  try {
+      await deleteDoc(doc(fireDB, 'products', item.id))
+      toast.success('Product Deleted successfully')
+      getProductData();
+      setLoading(false)
+  } catch (error) {
+      console.log(error)
+      setLoading(false)
+  }
+}
 
   return (
-    <myContext.Provider value={{mode, toggleMode, loading, setLoading}}>
+    <MyContext.Provider value={{mode, toggleMode, loading, setLoading, products, setProducts,addProduct, product, edithandle, updateProduct, deleteProduct}}>
         {props.children}
-    </myContext.Provider>
+    </MyContext.Provider>
   )
 }
 
